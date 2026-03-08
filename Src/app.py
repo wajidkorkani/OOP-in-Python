c705041fd32ac3543c7896cab12a1e0461752f92
wajidkorkani <errormrcoder@gmail.com>
2026-03-08 22:39:21 +0500

    Update class.

--- Src/app.py
+++ Src/app.py
@@ -1,21 +1,24 @@
 class Car:
-    def __init__(self):
-        self.name = ""
-        self.price = 0
-        self.color = ""
-        self.model = ""
+
+    def __init__(self, name, price, color, model):
+        self.name = name
+        self.price = price
+        self.color = color
+        self.model = model
+
+    def __del__(self):
+        print(f"The {self.name} is being scrapped. Goodbye!")
+
     def information(self):
         print(f"Car name: {self.name}")
         print(f"Car price: {self.price}")
         print(f"Car color: {self.color}")
         print(f"Car model: {self.model}")
+
     def start(self):
         print("Car started")
 
-car1 = Car()
-car1.name = "Toyota"
-car1.price = 20000
-car1.color = "Red"
-car1.model = "Corolla"
+
+car1 = Car("Toyota", 20000, "Red", "Corolla")
 car1.information()
 car1.start()
